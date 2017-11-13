9b8038d873fa9122b5e00544102e8a7543f94dcd
David Edwards <david.edwards@gmail.com>
2017-11-13 13:04:17 -0700

    closer

--- Scratch.py
+++ Scratch.py
@@ -3,10 +3,6 @@ import matplotlib
 import neuralnetworks as nn
 import mlutils as ml
 
-nnet = nn.NeuralNetworkClassifier
-
-nnet.train(X,T,)
-
 
 
 def trainNNs(X, T, trainFraction, hiddenLayerStructures, numberRepetitions, numberIterations, classify):
@@ -15,18 +11,30 @@ def trainNNs(X, T, trainFraction, hiddenLayerStructures, numberRepetitions, numb
     import matplotlib
     import neuralnetworks as nn
 
+    results = []
+
     # debugging
-    verbose = True
+    verbose = False
 
     for structure in hiddenLayerStructures:
-        Xtrain, Ttrain, Xvalidate, Tvalidate, Xtest, Ttest = partition(X, T, (0.6, 0.2, 0.2), classification=True)
         for i in range(numberRepetitions):
+            Xtrain, Ttrain, Xtest, Ttest = ml.partition(X, T, (trainFraction, 1 - trainFraction),
+                                                        classification=classify)
             if classify:
-                nnet = nn.NeuralNetworkClassifier
+                nnet = nn.NeuralNetworkClassifier(1,structure,1)
             else:
-                nnet = nn.NeuralNetwork
+                nnet = nn.NeuralNetwork(1,structure,1)
 
-            nnet.train(X, T, numberIterations, verbose)
+            nnet.train(Xtrain, Ttrain, numberIterations, verbose)
 
+            Y = nnet.use(X)
+            Ytest,Ztest = nnet.use(Xtest,allOutputs=True)
 
-results = trainNNs(X, T, trainFraction, hiddenLayerStructures, numberRepetitions, numberIterations, classify)
+            results.append([structure, Xtest, Ztest])
+
+    return results
+
+X = np.arange(10).reshape((-1,1))
+T = X + 1 + np.random.uniform(-1, 1, ((10,1)))
+results = trainNNs(X, T, 0.8, [2, 10, [10, 10]], 5, 100, classify=False)
+print(results)
